15a0bfcf46c8a9153f1e9a3638c7d57fb5613a1c
alsharmani1 <alsharmani1@gmail.com>
2019-05-22 17:55:40 -0400

    added frontend content with functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-    return ( 
-    <div>
-        Hi There!
-    </div>
-    );
-};
+import Header from './Header'
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
-export default App;
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    };
+
+    render() { 
+        return ( 
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        {/* exact or exact={true} to match urls */}
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route exact path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter >
+            </div>
+            );
+    }
+}
+
+export default connect(null, actions)(App);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);                     
+        res.send(req.user);                      
     });
 
     app.get('/', (req, res) => {
