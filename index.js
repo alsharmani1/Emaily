40838922047152dc92d9be5a7341f59a8fc67ea0
Sharmani <alsharmani1@gmail.com>
2019-07-02 16:10:24 -0400

    modified package

--- index.js
+++ index.js
@@ -29,5 +29,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve prod assets 
+    // main.js or main.css
+    app.use(express.static('client/build'));
+
+    // Express will serve index.html if
+    // route is not recognized
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
