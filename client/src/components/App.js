fd49b7918442dc56bb6942998b1e90c717703ac5
Sharmani <alsharmani1@gmail.com>
2019-07-16 20:56:05 -0400

    finished email sending and validation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing.js';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
     componentDidMount() {
@@ -15,17 +15,15 @@ class App extends Component {
 
     render() { 
         return ( 
-            <div className="container">
                 <BrowserRouter>
-                    <div>
+                    <div className="container">
                         {/* exact or exact={true} to match urls */}
                         <Header />
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/surveys" component={Dashboard} />
                         <Route exact path="/surveys/new" component={SurveyNew} />
                     </div>
-                </BrowserRouter >
-            </div>
+                </BrowserRouter>
             );
     }
 }
--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
     cookieKey: process.env.cookieKey,
     stripePublishableKey: process.env.stripePublishableKey,
     stripeSecretKey: process.env.stripeSecretKey,
-    sendGridKey: process.env.sendGridKey
+    sendGridKey: process.env.sendGridKey,
+    redirectDomain: process.env.redirectDomain
 }; 
