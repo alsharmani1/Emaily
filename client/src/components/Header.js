40b799017de5f747fd9e928a752336722ccdec94
alsharmani1 <alsharmani1@gmail.com>
2019-09-01 22:59:55 -0400

    minor links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,7 +18,8 @@ class Header extends Component {
         return [
           <li key="1"><Payments /></li>,
           <li key="2" style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits} </li>,
-          <li key="3"><a href="/api/logout">Logout</a></li> 
+          <li key="3"><a href="/surveys">Surveys</a></li>,
+          <li key="4"><a href="/api/logout">Logout</a></li> 
         ];
     }
   }
@@ -28,7 +29,7 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <Link 
-          to={this.props.auth ? '/surveys' : '/'} 
+          to={this.props.auth ? '/' : '/'} 
           className="left brand-logo"
           >
             Emaily
